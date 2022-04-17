4f32949b69833f7538c624a67844d5fc0f66ffa3
kyelebeniga <kyele.beniga@gmail.com>
2022-04-17 19:19:05 +0800

    More hero

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let counters = document.querySelectorAll("#found");
+let speed = 5;
+
+counters.forEach(counters =>{
+    let endValue = parseInt(counters.getAttribute("data-target"));
+    let startValue = 0
+    console.log(endValue);
+    let duration = Math.floor(speed / endValue);
+    let counter = setInterval(function(){
+        startValue += 1;
+        counters.textContent = startValue;
+        if (startValue == endValue){
+            clearInterval(counter);
+        }
+    }, duration);
+});
+
+let activated = document.querySelectorAll("#activated");
+let speed2 = 2000;
+
+activated.forEach(activated =>{
+    let endValue = parseInt(activated.getAttribute("data-target"));
+    let startValue = 0
+    console.log(endValue);
+    let duration = Math.floor(speed2 / endValue);
+    let counter = setInterval(function(){
+        startValue += 1;
+        activated.textContent = startValue;
+        if (startValue == endValue){
+            clearInterval(counter);
+        }
+    }, duration);
+});
