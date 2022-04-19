fb747d4087e84cb319ee3726a7b0ce78d1a6fcac
kyelebeniga <kyele.beniga@gmail.com>
2022-04-19 10:48:51 +0800

    Added featured article page

--- js/script.js
+++ js/script.js
@@ -39,3 +39,47 @@ window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 })
+
+//Career Carousel
+$('.carousel').slick({ 
+    arrows: true,
+    dots: false,
+    infinite: false,
+    prevArrow: '<button class="prev-arrow"><i class="fa-solid fa-chevron-left"></i></button>',
+    nextArrow: '<button class="next-arrow"><i class="fa-solid fa-chevron-right"></i></button>'
+});
+$('.carousel .prev-arrow').hide();
+$('.carousel').on('afterChange', function(){
+    console.log($('.carousel').slick('slickCurrentSlide'));
+    var currentSlide = $('.carousel').slick('slickCurrentSlide');
+if(currentSlide==0){
+    $('.carousel .prev-arrow').hide();
+    $('.carousel .next-arrow').show();
+}
+if(currentSlide==1){
+    $('.carousel .prev-arrow').show();
+    $('.carousel .next-arrow').hide();
+}
+});
+
+//News Carousel
+$('.news-carousel').slick({ 
+    arrows: true,
+    dots: false,
+    infinite: false,
+    prevArrow: '<button class="prev-arrow"><i class="fa-solid fa-chevron-left"></i></button>',
+    nextArrow: '<button class="next-arrow"><i class="fa-solid fa-chevron-right"></i></button>'
+});
+$('.news-carousel .prev-arrow').hide();
+$('.news-carousel').on('afterChange', function(){
+    console.log($('.news-carousel').slick('slickCurrentSlide'));
+    var currentSlide = $('.news-carousel').slick('slickCurrentSlide');
+if(currentSlide==0){
+    $('.news-carousel .prev-arrow').hide();
+    $('.news-carousel .next-arrow').show();
+}
+if(currentSlide==1){
+    $('.news-carousel .prev-arrow').show();
+    $('.news-carousel .next-arrow').hide();
+}
+});
